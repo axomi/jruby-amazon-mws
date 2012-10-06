da30d76cc351d04a0a1ce587fef8a4fd88e1eb01
Brian Sam-Bodden <bsbodden@integrallis.com>
2012-10-06 08:22:42 -0700

    Remove client's Amazon Creds from the repo (Oops)

--- examples/list_matching_products.rb
+++ examples/list_matching_products.rb
@@ -1,15 +1,18 @@
 $LOAD_PATH.unshift File.expand_path(File.join(File.dirname(__FILE__), "..", "lib")) 
 
-require 'jruby_amazon_mws' 
+require 'jruby_amazon_mws'  
+require 'yaml'
+
+config = YAML::load(File.open("amazon-credentials.yaml"))
 
 client = AmazonMWS::Client.new(
-  :locale => 'US',
-  :key => 'AKIAJ267FAM22J74RT7A',
-  :secret => '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',
-  :marketplace => 'ATVPDKIKX0DER',
-  :seller => 'A3G6WNDFMU2LTW',
-  :application_name => 'integrallis_jruby_amazon_mws_client',
-  :application_version => '0.0.1'
+  :locale => config['locale'],
+  :key => config['key'],
+  :secret => config['secret'],
+  :marketplace => config['marketplace'],
+  :seller => config['seller'],
+  :application_name => config['application_name'],
+  :application_version => config['application_version']
 )  
 
 matching_products = client.list_matching_products('Harry Potter')       
--- examples/lowest_offer_listings_for_asin.rb
+++ examples/lowest_offer_listings_for_asin.rb
@@ -1,16 +1,19 @@
 $LOAD_PATH.unshift File.expand_path(File.join(File.dirname(__FILE__), "..", "lib")) 
 
 require 'jruby_amazon_mws' 
+require 'yaml'
+
+config = YAML::load(File.open("amazon-credentials.yaml"))
 
 client = AmazonMWS::Client.new(
-  :locale => 'US',
-  :key => 'AKIAJ267FAM22J74RT7A',
-  :secret => '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',
-  :marketplace => 'ATVPDKIKX0DER',
-  :seller => 'A3G6WNDFMU2LTW',
-  :application_name => 'integrallis_jruby_amazon_mws_client',
-  :application_version => '0.0.1'
-)  
+  :locale => config['locale'],
+  :key => config['key'],
+  :secret => config['secret'],
+  :marketplace => config['marketplace'],
+  :seller => config['seller'],
+  :application_name => config['application_name'],
+  :application_version => config['application_version']
+) 
 
 lowest = client.lowest_offer_for_asin('059035342X')       
 puts "The lowest price is ==> #{lowest}"
